014e3a46b935086256176f2cf694502a5cf0d42b
Moos Vanlangendonck <r0879857@student.thomasmore.be>
2024-11-01 15:19:48 +0100

    start

--- BookScanner/obj/Debug/net8.0-android/BookScanner.AssemblyInfo.cs
+++ BookScanner/obj/Debug/net8.0-android/BookScanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookScanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+30b18576e83b0e32c5ed96e7a78457680c85daeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookScanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookScanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
